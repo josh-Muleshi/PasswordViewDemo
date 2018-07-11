cd401afe347a2e7fb3c6431884015ad6c47a9418
zxk <zhangxiekun@gmail.com>
2018-07-11 20:15:17 +0800

    Initial commit

---
+++ app/src/main/java/demo/zxk/passwordviewdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package demo.zxk.passwordviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PasswordView passwordView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        passwordView = (PasswordView)findViewById(R.id.passwordView);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, passwordView.getPassword(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
